4eebb4d7876feaabb2dd9a99d5feca71ee4bdef0
MatteoZattini <matteozattini@hotmail.com>
2024-05-06 16:32:23 +0200

    m0

---
+++ main.js
@@ -0,0 +1,10 @@
+let ourTeam = [
+    { nome: "Wayne Barnett", roulo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg" },
+    { nome: "Angela Carol", roulo: "Chief Editor", immagine: "angela-caroll-chief-editor.jpg" },
+    { nome: "Walter Gordon", roulo: "Office Manager", immagine: "walter-gordon-office-manager.jpg" },
+    { nome: "Angela Lopez", roulo: "Social Media Manager", immagine: "angela-lopez-social-media-manager.jpg" },
+    { nome: "Scott Estrada", roulo: "Developer", immagine: "scott-estrada-developer.jpg" },
+    { nome: "Barbara Ramos", roulo: "Graphic Designer", immagine: "barbara-ramos-graphic-designer.jpg" }
+]
+
+console.log(ourTeam)
